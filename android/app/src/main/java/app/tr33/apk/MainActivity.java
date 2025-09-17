a06dd50264773beb507d48c78bd9f8dd0fa15338
leonelpb <leonelperez.23@gmail.com>
2025-09-17 11:02:02 -0300

    ADD:Android, Commit before deploy

---
+++ android/app/src/main/java/app/tr33/apk/MainActivity.java
@@ -0,0 +1,5 @@
+package app.tr33.apk;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,25 +1,13 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.6d78062608564321a2a1ac733fe4e71c',
-  appName: 'Tr33 - A Lovable project',
+  appId: 'com.tr33.app',
+  appName: 'Tr33',
   webDir: 'dist',
-  server: {
-    url: 'https://6d780626-0856-4321-a2a1-ac733fe4e71c.lovableproject.com?forceHideBadge=true',
+/*   server: {
+    url: 'http://172.25.160.1:8080',
     cleartext: true
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 0,
-      launchAutoHide: true,
-      backgroundColor: "#ffffff",
-      androidSplashResourceName: "splash",
-      androidScaleType: "CENTER_CROP",
-      showSpinner: false,
-      splashFullScreen: true,
-      splashImmersive: true
-    }
-  }
+  }, */
 };
 
 export default config;
